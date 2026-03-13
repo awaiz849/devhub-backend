981d57f215887da0fadde351acfe8d6d155d9773
Mohammad Awaiz <mohd.awaiz388@gmail.com>
2026-03-13 23:21:09 +0530

    day 4 completed

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,19 +4,24 @@ const authMiddleware = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization
 
+    // Check if Authorization header exists
     if (!authHeader) {
       return res.status(401).json({ message: "No token provided" })
     }
 
+    // Extract token from "Bearer TOKEN"
     const token = authHeader.split(" ")[1]
 
+    // Verify the token
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-    req.user = decoded
+    // Attach user ID to request object
+    req.user = decoded.id
 
+    // Move to next middleware / controller
     next()
   } catch (error) {
-    res.status(401).json({ message: "Invalid token" })
+    return res.status(401).json({ message: "Invalid or expired token" })
   }
 }
 
